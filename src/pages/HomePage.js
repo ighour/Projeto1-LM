4aab726fee1d1eceaa8cb7e709ca22fa7e5b6811
ighour <ti.celio.rodrigues@gmail.com>
2020-10-26 13:47:30 +0000

    Set needed features.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,16 +4,12 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import MainFeaturedPost from '../components/homepage/MainFeaturedPost';
-import FeaturedPost from '../components/homepage/FeaturedPost';
+// import FeaturedPost from '../components/homepage/FeaturedPost';
 
 const useStyles = makeStyles((theme) => ({
     mainGrid: {
       marginTop: theme.spacing(3),
       paddingBottom: theme.spacing(3)
-    },
-    post: {
-      ...theme.typography.body2,
-      padding: theme.spacing(3, 0),
     },
     postLeft: {
       marginBottom: theme.spacing(5)
@@ -39,24 +35,24 @@ const mainFeaturedPost = {
     linkText: 'Continue reading…',
   };
   
-  const featuredPosts = [
-    {
-      title: 'Featured post',
-      date: 'Nov 12',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-      image: 'https://source.unsplash.com/random',
-      imageText: 'Image Text',
-    },
-    {
-      title: 'Post title',
-      date: 'Nov 11',
-      description:
-        'This is a wider card with supporting text below as a natural lead-in to additional content.',
-      image: 'https://source.unsplash.com/random',
-      imageText: 'Image Text',
-    },
-  ];
+  // const featuredPosts = [
+  //   {
+  //     title: 'Featured post',
+  //     date: 'Nov 12',
+  //     description:
+  //       'This is a wider card with supporting text below as a natural lead-in to additional content.',
+  //     image: 'https://source.unsplash.com/random',
+  //     imageText: 'Image Text',
+  //   },
+  //   {
+  //     title: 'Post title',
+  //     date: 'Nov 11',
+  //     description:
+  //       'This is a wider card with supporting text below as a natural lead-in to additional content.',
+  //     image: 'https://source.unsplash.com/random',
+  //     imageText: 'Image Text',
+  //   },
+  // ];
   
 function HomePage() {
     const classes = useStyles();
@@ -70,59 +66,74 @@ function HomePage() {
                 Project
               </Typography>
               <Divider />
-              <div className={classes.post}>
-                Lorem Ipsum...
-              </div>
-              <Grid container spacing={4}>
+              <p>
+                Teaser | About
+              </p>
+              <p>
+                Team
+              </p>
+              {/* <Grid container spacing={4}>
                 {featuredPosts.map((post) => (
                   <FeaturedPost key={post.title} post={post} />
                 ))}
-              </Grid>
+              </Grid> */}
             </Grid>
             <Grid item xs={12} className={classes.postRight}>
               <Typography variant="h5" gutterBottom className={classes.postTitleRight}>
                 Pitch
               </Typography>
               <Divider />
-              <div className={classes.post}>
-                Lorem Ipsum...
-              </div>
+              <p>
+                What
+              </p>
+              <p>
+                Why
+              </p>
+              <p>
+                Who
+              </p>
+              <p>
+                How
+              </p>
+              <p>
+                UX Mapping
+              </p>
             </Grid>
             <Grid item xs={12} className={classes.postLeft}>
               <Typography variant="h5" gutterBottom className={classes.postTitle}>
-                Prototype
+              Bibliographic research 
               </Typography>
               <Divider />
-              <div className={classes.post}>
-                Lorem Ipsum...
-              </div>
+              <p>
+               ...
+              </p>
             </Grid>
             <Grid item xs={12} className={classes.postRight}>
               <Typography variant="h5" gutterBottom className={classes.postTitleRight}>
-                Report
+                Resources
               </Typography>
               <Divider />
-              <div className={classes.post}>
-                Lorem Ipsum...
-              </div>
+              <p>
+               Report (PDF)
+              </p>
+              <p>
+               Benchmarking
+              </p>
+              <p>
+              dissemination materials
+              </p>
+              <p>
+              link to product or demo
+              </p>
             </Grid>
             <Grid item xs={12} className={classes.postLeft}>
               <Typography variant="h5" gutterBottom className={classes.postTitle}>
                 Gallery
               </Typography>
               <Divider />
-              <div className={classes.post}>
-                Lorem Ipsum...
-              </div>
-            </Grid>
-            <Grid item xs={12} className={classes.postRight}>
-              <Typography variant="h5" gutterBottom className={classes.postTitleRight}>
-                Team
-              </Typography>
-              <Divider />
-              <div className={classes.post}>
-                Lorem Ipsum...
-              </div>
+              <p>
+                ...
+              </p>
             </Grid>
           </Grid>
       </React.Fragment>
